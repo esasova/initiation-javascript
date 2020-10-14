ce45653bd27f1a3569e39c7b6735f7a7aa4f8653
esasova <sassova.elena@gmail.com>
2020-10-14 10:53:53 +0200

    first try

---
+++ index.js
@@ -0,0 +1,11 @@
+let prenom = 'Rama';
+let nombre = 10.5;
+let booleenne = true;
+
+prenom = 'Jean';
+
+console.log(prenom);
+
+let elementParagraphe = document.querySelector ('#para')
+
+elementParagraphe.textContent = prenom
