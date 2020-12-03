94e089268a9c13fa3d0bf7c8dfbaab9a50560ee3
esasova <sassova.elena@gmail.com>
2020-12-03 17:03:53 +0100

    files organisation

---
+++ js/piramide.js
@@ -0,0 +1,18 @@
+for (x=2; x<101; x++){
+    let result = x / y;
+}
+
+let etoile = "";
+for (let x=0; x<6; x++){
+    etoile +="*"
+    console.log(etoile);
+} 
+
+let etoile = "";
+for (let x=0; x<9; x++){
+    etoile +=x
+    console.log(etoile);
+} 
+
+
+
