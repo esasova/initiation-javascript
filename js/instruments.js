0e0d260276661259425369d7fcdf351ba5a29a13
esasova <sassova.elena@gmail.com>
2020-12-09 17:23:19 +0100

    gestion des classes

--- js/instruments.js
+++ js/instruments.js
@@ -5,4 +5,22 @@ console.log(links.length);
 console.log(links[0].href);
 //trouve la position du dernier lien et l'affiche
 let lastItem = links.length - 1;
-console.log(links[lastItem].href)
+console.log(links[lastItem].href);
+
+// fonction qui vérifie qu'un élément possède une classe
+const possede = (instrument, classe) => {
+    let classes = document.getElementById(instrument);
+    if (classes !== null){
+            console.log(classes.classList.contains(classe))
+        } 
+    else {
+        console.log('mauvais instrument')
+    }
+    
+        
+    
+}
+possede("saxophone", "bois"); // Doit afficher true
+possede("saxophone", "cuivre"); // Doit afficher false
+possede("trompette", "cuivre"); // Doit afficher true
+possede("contrebasse", "cordes"); // Doit afficher une erreur
