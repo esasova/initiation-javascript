3683f08300928114725f6241d774f0ab2d875577
esasova <sassova.elena@gmail.com>
2020-12-11 16:57:05 +0100

    test1

--- js/langages.js
+++ js/langages.js
@@ -1,3 +1,8 @@
 let paraElt = document.createElement('p');
 let lienElt = document.createElement('a');
-lienElt.href = 'https://fr.wikipedia.org/wiki/Liste_des_langages_de_programmation'
+lienElt.href = 'https://fr.wikipedia.org/wiki/Liste_des_langages_de_programmation';
+lienElt.textContent = 'liste';
+paraElt.appendChild(document.createTextNode('En voici une '));
+paraElt.appendChild(lienElt);
+paraElt.appendChild(document.createTextNode(' plus complète'));
+console.log(paraElt);
