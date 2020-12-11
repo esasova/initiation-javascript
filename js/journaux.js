3fbd896487c33f0c794af4b3189ba8ba9d032a4d
esasova <sassova.elena@gmail.com>
2020-12-11 17:02:07 +0100

    files

---
+++ js/journaux.js
@@ -0,0 +1,4 @@
+// Liste des journaux
+var journaux = ["http://lemonde.fr", "http://lefigaro.fr", "http://liberation.fr"];
+
+// TODO : ajouter la liste des journaux sur la page, dans la div "contenu"
--- js/langages.js
+++ js/langages.js
@@ -1,7 +1,12 @@
+// création des éléments
 let paraElt = document.createElement('p');
 let lienElt = document.createElement('a');
+
+// définition de l'attribut et du texte du lien
 lienElt.href = 'https://fr.wikipedia.org/wiki/Liste_des_langages_de_programmation';
 lienElt.textContent = 'liste';
+
+//assemblage des éléménets html
 paraElt.appendChild(document.createTextNode('En voici une '));
 paraElt.appendChild(lienElt);
 paraElt.appendChild(document.createTextNode(' plus complète'));
