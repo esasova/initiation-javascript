c4780faffc1b2f37f59f6a622ca55d040adbe5e8
esasova <sassova.elena@gmail.com>
2020-12-14 15:20:22 +0100

    liste des journaux

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Différents exercices de JS trouvés sur internet ou suggerés dans la classe :
 - calculatrice (fonctions, opérateurs ariphmétiques)
 - variables et types
 - events, jeu (récupération de la position de la souris, changement du contenu)
-- parcourir le DOM (classes, id, balises, attributs)
+- parcourir le DOM (classes, id, balises, attributs, éléments de tableau)
 - piramide (boucle for)
 - getNumber (async/await)
 - chenger le DOM
--- js/journaux.js
+++ js/journaux.js
@@ -1,4 +1,15 @@
 // Liste des journaux
-var journaux = ["http://lemonde.fr", "http://lefigaro.fr", "http://liberation.fr"];
+const journaux = ["http://lemonde.fr", "http://lefigaro.fr", "http://liberation.fr"];
 
-// TODO : ajouter la liste des journaux sur la page, dans la div "contenu"
+//récupération de l'élémént parent
+const contenu = document.getElementById('contenu');
+//création de la liste dans l'élément parent
+const liste = document.createElement('ul');
+contenu.appendChild(liste);
+
+//récupération des éléments du tableau
+for(let i = 0; i < journaux.length; i++){
+let journal = document.createElement('li');
+journal.textContent = journaux[i];
+liste.appendChild(journal)
+}
